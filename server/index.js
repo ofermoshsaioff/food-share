c49d93a041860be278a5aa6bb168077e4474abbb
michael <mderazon@gmx.com>
2012-09-22 06:37:53 -0400

    adding missing semicolon and trying to push first time

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-var handle = {}
+var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/home"] = requestHandlers.start;
 handle["/about"] = requestHandlers.about;
-handle["/contact"] = requestHandlers.contact
+handle["/contact"] = requestHandlers.contact;
 server.start(router.route, handle);
